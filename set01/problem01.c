5f678430b6e3c05765626c7c20807f36ca09c70c
Kashchit <kashchitb.bsc23@rvu.edu.in>
2024-01-29 10:38:04 +0530

    test

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,7 +1,7 @@
 // 1. Write a C program to print your name.
 #include <stdio.h>
 int main(){
-    char name[100];
+    char name[50];
     printf("What is your name ? ");
     scanf("%s",&name);
     printf("Hello, %s",name);
