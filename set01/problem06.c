fa8d2fc312c1959db03ac7a0ad406a07236d956a
Kashchit B Thapa <kashchitb.bsc23@rvu.edu.in>
2024-01-30 09:16:56 +0530

    test

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,1 +1,34 @@
 //6. Write a C program to compare three numbers using **pass by reference**//
+#include <stdio.h>
+int input();
+void compare(int a, int b, int c, int *largest);
+void output(int a, int b, int c, int largest);
+int main(){
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    compare(a,b,c,&largest);
+    output(a,b,c,largest);
+    return 0;
+}
+int input(){
+    int x;
+    printf("Enter the number :");
+    scanf("%d",&x);
+    return x;
+}
+void compare(int a, int b, int c, int *largest){
+    if(a>b && a>c){
+        *largest = a;
+    }
+    else if(b>a && b>c){
+        *largest = b;
+    }
+    else{
+        *largest = c;
+    }
+}
+void output(int a, int b, int c, int largest){
+    printf("Largest between %d, %d and %d is : %d",a,b,c,largest);
+}
