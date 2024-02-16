88b9b17f1d4b375d82a5a152948024a19e71c910
Kashchit B Thapa <kashchitb.bsc23@rvu.edu.in>
2024-02-16 14:10:57 +0530

    test

--- set02/problem05.c
+++ set02/problem05.c
@@ -3,6 +3,34 @@
 int input();
 int find_gcd(int a, int b);
 void output(int a, int b, int gcd);
-int main(){
 
+int main() {
+    int a, b;
+    a = input();
+    b = input();
+    int gcd_result = find_gcd(a, b);
+    output(a, b, gcd_result);
+    return 0;
+}
+
+
+int input() {
+    int x;
+    printf("Enter the number: ");
+    scanf("%d", &x);
+    return x;
+}
+
+int find_gcd(int a, int b) {
+    int temp;
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void output(int a, int b, int gcd) {
+    printf("The GCD of %d and %d is %d\n", a, b, gcd);
 }
