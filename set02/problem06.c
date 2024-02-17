9ce2e63dfc802072b7f9dbe7f1a91df5af1042f4
Kashchit B Thapa <kashchitb.bsc23@rvu.edu.in>
2024-02-17 21:30:44 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -3,4 +3,37 @@
 void input_string(char *a);
 void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
- 
+int main(){
+     char str[100], rev_str[100];
+    input_string(str);
+    str_reverse(str, rev_str);
+    output(str, rev_str);
+
+    return 0;
+    }
+
+void input_string(char *a) {
+    printf("Enter a string: ");
+    scanf("%s", a);
+}
+
+void str_reverse(char *str, char *rev_str) {
+    int length = 0;
+    char *temp = str;
+
+    while (*temp != '\0') {
+        length++;
+        temp++;
+    }
+
+    for (int i = 0; i < length; i++) {
+        rev_str[i] = str[length - i - 1];
+    }
+    rev_str[length] = '\0'; 
+}
+
+void output(char *a, char *reverse_a) {
+    printf("Original String: %s\n", a);
+    printf("Reversed String: %s\n", reverse_a);
+}
+
